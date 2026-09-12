2fdb8f55edda2ab9fa9ceea69ebc1631a2a9ca4f
Lance <lanceandreiducante@gmail.com>
2026-09-12 10:33:55 +0800

    added scrolling animation

--- script.js
+++ script.js
@@ -202,3 +202,24 @@ window.addEventListener('resize', () => {
     answer.style.maxHeight = `${answer.scrollHeight}px`;
   });
 });
+
+const animatedItems = document.querySelectorAll('.animate-item');
+
+const animationObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+animatedItems.forEach((item) => {
+  animationObserver.observe(item);
+});
